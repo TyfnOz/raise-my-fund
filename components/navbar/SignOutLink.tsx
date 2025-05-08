4a9e7a3d77ec46dcdb52132e7faa2b597d66750f
Tayfun Özdemir <tyfnzdmr0@gmail.com>
2025-05-08 15:54:03 +0300

    login register using clerk

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,19 @@
+'use client';
+import { SignOutButton } from "@clerk/nextjs";
+import { toast } from "sonner";
+
 function SignOutLink() {
+  const handleLogout = () => {
+    toast('You have been signed out');
+  };
+
   return (
-    <div>SignOutLink</div>
-  )
+    <SignOutButton redirectUrl="/">
+      <button className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 }
 
-export default SignOutLink
+export default SignOutLink;
