065c8aca240ac6d7f75be6dda7d347a6bcf94f11
Tayfun Özdemir <tyfnzdmr0@gmail.com>
2025-05-15 14:59:51 +0300

    deployment fix

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,15 +1,11 @@
 import Link from 'next/link';
-import { GiTakeMyMoney } from "react-icons/gi";
-import { FcMoneyTransfer } from "react-icons/fc";
-
-import { Button } from '../ui/button';
 import Image from 'next/image';
 
 function Logo() {
   return (
-    <a href="/">
+    <Link href="/">
       <Image src='/logo.png' alt='logo' width={100} height={100}/>
-    </a>
+    </Link>
   );
 }
 
